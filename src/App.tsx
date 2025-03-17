62810b6c54bfecf5495237ba99352b08ee99217f
bhavesh bambhaniya <bhavesh.bam@gmail.com>
2025-03-17 11:48:48 +0530

    Add Routes for Frontend.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './components/Authentication/LoginPage';
 import RegistrationPage from './components/Authentication/RegistrationPage';
 import AuthLayout from './components/Authentication/AuthLayout';
+import DashboardLayout from './components/DashboardLayout/DashboardLayout';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import ClassesPage from './components/ClassesPage/ClassesPage';
+import AddClassPage from './components/AddClass/AddClassPage';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'></header>
 			<BrowserRouter>
 				<Routes>
 					<Route element={<AuthLayout />}>
@@ -21,9 +24,45 @@ function App() {
 							element={<RegistrationPage />}
 						/>
 					</Route>
+					<Route element={<ProtectedRoute />}>
+						<Route
+							path='/app'
+							element={<DashboardLayout />}
+						>
+							<Route
+								path='classes'
+								element={<ClassesPage />}
+							/>
+							<Route
+								path='classes/new'
+								element={<AddClassPage />}
+							/>
+							<Route
+								path='tasks'
+								element={<div>Tasks</div>}
+							/>
+							<Route
+								path='notifications'
+								element={<div>Notifications</div>}
+							/>
+							<Route
+								path='settings'
+								element={<div>Settings</div>}
+							/>
+							<Route
+								path='*'
+								element={<ClassesPage />}
+							/>
+						</Route>
+					</Route>
 					<Route
-						path='/'
-						element={<LoginPage />}
+						path='*'
+						element={
+							<Navigate
+								to='/login'
+								replace
+							/>
+						}
 					/>
 				</Routes>
 			</BrowserRouter>
